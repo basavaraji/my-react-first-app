e4ae8dae23d235a3d30e74c9d0801aa5914a7583
Basavaraj R <basavarajr1999@gmail.com>
2025-11-13 16:18:20 +0530

    Updates recent codes in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 function Demo(){
-  return (<p>Java Full Stack Developer</p>)
+  return (
+    <div>
+      <h1>My First Project</h1>
+      <p>I uploaded React Project folder in Git</p>
+    </div>
+  )
 }
 export default Demo;
